46fd613220cfaf58bf2430c41541fe2843bc3a1c
malloryjustis <98776627+malloryjustis@users.noreply.github.com>
2023-02-21 11:44:54 -0500

    Update battery_charging.py

--- battery_charging.py
+++ battery_charging.py
@@ -11,14 +11,31 @@ battery_1_signal = #import signal from battery1
 
 if battery_1_signal >= max_battery_limit:
   #close circuit to battery 1
+  print('battery 1 charge completed')
   
+  if battery_2_signal < max_battery_limit:
   #open circuit to battery 2
   
 else return
 
 if battery_2_signal >= max_battery_limit:
     #close circuit to battery 2
+    print('battery 2 charge completed')
     
+    if battery_3_signal < max_battery_limit:
     #open circuit to battery 3
     
 else #keep running current to battery 2
+
+if battery_3_signal >= max_battery_limit:
+  # close circuit to battery 3
+  print('battery 3 charge completed')
+  
+  if battery_4_signal < max_battery_limit:
+  #open circuit to battery 4
+  
+if battery_4_signal >= max_battery_limit:
+  #close circuit to battery 4
+  print('battery 4 charge completed')
+  
+
