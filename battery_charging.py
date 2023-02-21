fa969c1a8d62acfb2e06e42354c13930fb8a2f63
malloryjustis <98776627+malloryjustis@users.noreply.github.com>
2023-02-20 21:54:07 -0500

    Create battery_charging.py

---
+++ battery_charging.py
@@ -0,0 +1,24 @@
+#import things need to import
+
+#use crontab?
+
+#run through this every _____ minutes
+
+#define maximum battery limit
+max_battery_limit = 
+
+battery_1_signal = #import signal from battery1
+
+if battery_1_signal >= max_battery_limit:
+  #close circuit to battery 1
+  
+  #open circuit to battery 2
+  
+else return
+
+if battery_2_signal >= max_battery_limit:
+    #close circuit to battery 2
+    
+    #open circuit to battery 3
+    
+else #keep running current to battery 2
