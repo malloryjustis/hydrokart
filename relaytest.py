c4aab0ba66cb0f42baa9d058b91dc9b4280bf6e0
malloryjustis <98776627+malloryjustis@users.noreply.github.com>
2023-03-30 13:28:03 -0400

    Update relaytest.py

--- relaytest.py
+++ relaytest.py
@@ -1,9 +1,5 @@
 
-#setting the pins that will be used for the circuit
-pinMode(13, OUTPUT)
-pinMode(12, OUTPUT)
 pinMode(8, OUTPUT)
-pinMode(7, OUTPUT)
 
-digitalWrite(13, High)
+digitalWrite(8, HIGH)
 
